2a02b83da711860731abc124e3baefbe15228618
duytran-vio <65882917+duytran-vio@users.noreply.github.com>
2022-04-18 11:48:18 +0700

    add buzzer button

--- frontend/smartdoor/src/components/Top.js
+++ frontend/smartdoor/src/components/Top.js
@@ -2,12 +2,18 @@ import React from 'react'
 
 const Top = () => {
   return (
-    <div className='value_box'>
-      <div style={{fontSize: "50px", marginRight: "30px"}}>
-        Số người trong phòng: 
+    <div className='row1'>
+      <div className='value_box'>
+        <div style={{fontSize: "50px", marginRight: "30px"}}>
+          Số người trong phòng: 
+        </div>
+        <div className='value'>
+          <h1>5/5</h1>
+        </div>
+        
       </div>
-      <div className='value'>
-        <h1>5/5</h1>
+      <div className='buzzer'>
+        <button className='button-19'>Buzzer</button>
       </div>
     </div>
   )
